e6783327879a5aee00b4b100f12c9caee63098c1
Daniel Jones <McSwaggens@protonmail.com>
2017-01-23 00:12:00 +1100

    Harvester now goes to closest source

--- role.harvester.js
+++ role.harvester.js
@@ -5,8 +5,9 @@ var roleHarvester = {
         if(creep.carry.energy !== creep.carryCapacity) 
         {   
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+			var closest = creep.pos.findClosestByRange(sources);
+            if(creep.harvest(closest) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(closest);
                 creep.say('Collecting')
             }
         }
