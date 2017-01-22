3f36d650dba27584fff16205fb91ac2ccf3d2b40
Daniel Jones <McSwaggens@protonmail.com>
2017-01-22 23:46:37 +1100

    Fixed autism

--- misc.js
+++ misc.js
@@ -6,7 +6,7 @@ function GetCost (parts)
 	var cost = 0;
 	for (var i = 0; i < parts.length; i++)
 	{
-	  var part = parts;
+	  var part = parts[i];
 	  cost += BODYPART_COST[part];
 	}
 	return cost;
