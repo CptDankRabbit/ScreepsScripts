f6e72cbe90d4834059da5bb50152d36ded73a68f
Tyso <tyson100roxs@hotmail.com>
2017-08-09 15:30:48 +1000

    Harvesters Build Instead of Idle.

--- role.harvester.js
+++ role.harvester.js
@@ -87,7 +87,14 @@ var roleHarvester = {
                 }
                 else
                 {
-                    creep.moveTo(Game.flags.Idle);
+                   var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        
+			        if(targets.length) 
+        			{
+				        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+					    creep.moveTo(targets[0]);
+				        }
+			        }
                 }
             }
             
